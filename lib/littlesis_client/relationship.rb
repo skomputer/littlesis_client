46b0b757538fbb98fd02448f2c20a1e01f4cdf94
Matthew Skomarovsky <matthew@verypolite.com>
2015-01-06 19:15:22 -0500

    models replace empty strings with nil

--- lib/littlesis_client/relationship.rb
+++ lib/littlesis_client/relationship.rb
@@ -51,6 +51,7 @@ class LittlesisClient::Relationship < LittlesisClient::Model
   
   def set_data(data)
     data.each do |k, v|
+      v = nil if v == ""
       method = (k.to_s + "=").to_sym
       if respond_to? method
         send(method, self.class.symbolize_keys(v))
